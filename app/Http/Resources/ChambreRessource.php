411ad3be347143b42709bccae8e3c7a9a75a45c7
Tidiane Badji <btidianed004@gmail.com>
2024-02-09 00:29:43 +0000

    feat: review code

---
+++ app/Http/Resources/ChambreRessource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ChambreRessource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'libelle' =>  $this->libelle,
+            'type_chambre'=> $this->type_chambre,
+            'nombres_lits'=>$this->type_chambre,
+            'nombres_limites'=>$this->nombres_limites,
+            'pavillon' =>$this->pavillons->libelle
+        ];
+    }
+}
