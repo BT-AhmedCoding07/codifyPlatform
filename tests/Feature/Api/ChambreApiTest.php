647b2d6407934580def3a99ad66f025e8c62eb54
Tidiane Badji <btidianed004@gmail.com>
2024-03-05 09:27:41 +0000

    feat: test ok

--- tests/Feature/Api/ChambreApiTest.php
+++ tests/Feature/Api/ChambreApiTest.php
@@ -20,7 +20,7 @@ class ChambreApiTest extends TestCase
             'password' => 'Admin123@',
         ]);
         $response = $this->post('api/admin/chambre/create/1', [
-            'libelle'=> 'chambre A1',
+            'libelle'=> 'A chambre 10',
             'type_chambre'=> 'Partagée',
             'nombres_lits'=> 4,
             'nombres_limites'=> 8,
@@ -36,8 +36,8 @@ class ChambreApiTest extends TestCase
             'email' => 'admin@gmail.com',
             'password' => 'Admin123@',
         ]);
-        $response = $this->put('api/admin/chambre/update/{id}', [
-            'libelle'=> 'chambre A1',
+        $response = $this->put('api/admin/chambre/update/1', [
+            'libelle'=> 'A chambre 01 ',
             'type_chambre'=> 'Partagée',
             'nombres_lits'=> 2,
             'nombres_limites'=> 4,
@@ -51,17 +51,17 @@ class ChambreApiTest extends TestCase
             'email' => 'email@email.com',
             'password' => 'password',
         ]);
-        $response = $this->delete('api/admin/chambre/delete/{id}');
+        $response = $this->delete('api/admin/chambre/delete/22');
         $response->assertStatus(200);
     }
 
     public function test_show_a_chambre_by_id(): void
     {
         $response = $this->post('/api/auth/login', [
-            'email' => 'email@email.com',
+            'email' => 'admin@gmail.com',
             'password' => 'password',
         ]);
-        $response = $this->get('api/chambre/read/{id}');
+        $response = $this->get('api/chambre/read/1');
         $response->assertStatus(200);
     }
     //ADMIN
@@ -71,7 +71,7 @@ class ChambreApiTest extends TestCase
             'email' => 'email@email.com',
             'password' => 'password',
         ]);
-        $response = $this->get('api/admin/chambre/read/{id}');
+        $response = $this->get('api/admin/chambre/read/9');
         $response->assertStatus(200);
     }
     public function test_listes_chambre(): void
@@ -80,7 +80,7 @@ class ChambreApiTest extends TestCase
             'email' => 'email@email.com',
             'password' => 'password',
         ]);
-        $response = $this->get('api/chambres');
+        $response = $this->get('api/admin/chambres');
         $response->assertStatus(200);
     }
 }
--- tests/Feature/Api/UserApiTest.php
+++ tests/Feature/Api/UserApiTest.php
@@ -10,16 +10,16 @@ use Illuminate\Support\Facades\Hash;
 
 class UserApiTest extends TestCase
 {
-    // public function test_can_login_user_with_email()
-    // {
-    //     $userData = [
-    //         'email' => 'admin@gmail.com',
-    //         'password' => 'Admin123@'
-    //     ];
-    //     $response = $this->postJson('/api/auth/login', $userData);
-    //     $response->assertStatus(200);
+    public function test_can_login_user_with_email()
+    {
+        $userData = [
+            'email' => 'admin@gmail.com',
+            'password' => 'Admin123@'
+        ];
+        $response = $this->postJson('/api/auth/login', $userData);
+        $response->assertStatus(200);
 
-    // }
+    }
 
 
     public function test_user_can_logout()
@@ -33,5 +33,5 @@ class UserApiTest extends TestCase
         $this->assertGuest();
         $response->assertStatus(200);
         // dump($response->json());
-    }
+   }
 }
